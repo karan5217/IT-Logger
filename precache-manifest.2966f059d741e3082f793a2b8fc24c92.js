e1db76a13568aaab585101ae147581cb8b75b52b
karan5217 <karan5217@gmail.com>
2020-08-10 10:30:53 +0530

    Updates

---
+++ precache-manifest.2966f059d741e3082f793a2b8fc24c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22332d20784b34b9fa1ed0d09967caa0",
+    "url": "/karan5217/it-logger/index.html"
+  },
+  {
+    "revision": "ddb3e33fc1729484f207",
+    "url": "/karan5217/it-logger/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "894bb53f3739b6fb9876",
+    "url": "/karan5217/it-logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ddb3e33fc1729484f207",
+    "url": "/karan5217/it-logger/static/js/2.dd804ba2.chunk.js"
+  },
+  {
+    "revision": "494382bc2e1fa58818bf1f9041d8a9bc",
+    "url": "/karan5217/it-logger/static/js/2.dd804ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894bb53f3739b6fb9876",
+    "url": "/karan5217/it-logger/static/js/main.d976c514.chunk.js"
+  },
+  {
+    "revision": "9b68606b17a1c8bb5043",
+    "url": "/karan5217/it-logger/static/js/runtime-main.cbba5c9e.js"
+  }
+]);
